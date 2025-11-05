b74efda83bee6b804bdb74d918bbbd5514138527
omdxp <omar@omdxp.com>
2025-11-05 21:03:21 +0100

    feat: Add Algolia site verification meta tag to configuration

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -29,6 +29,16 @@ const config: Config = {
 
   onBrokenLinks: "throw",
 
+  headTags: [
+    {
+      tagName: "meta",
+      attributes: {
+        name: "algolia-site-verification",
+        content: "DEE59269C754EA61",
+      },
+    },
+  ],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
